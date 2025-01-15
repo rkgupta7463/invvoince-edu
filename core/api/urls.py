3ee223624bac98e3a8d389a9afada08b3d18540e
Rishu Gupta <rishukumargupta8409@gmail.com>
2025-01-15 22:56:24 +0530

    change update

--- core/api/urls.py
+++ core/api/urls.py
@@ -11,7 +11,7 @@ urlpatterns = [
     path("",Home.as_view()),
     path('api-auth/', include('rest_framework.urls')),
     path('login/', obtain_auth_token, name='login'),
-    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('token/verify/', TokenVerifyView.as_view(), name='token_verify'),
     path('logout/', logout_user, name='logout'),
